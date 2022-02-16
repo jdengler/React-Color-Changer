cddeac8b7d02d651a57b59ec7a636958ee4c71d6
Josh Engler <55295030+jdengler@users.noreply.github.com>
2022-02-16 09:42:07 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import ColorButton from './ColorButton'
+
+function App() {
+  const [color, setColor, logoColor] = useState('')
+  return (
+      <div className={`react-root ${color}`}>
+        <div className='centered'>
+        <div id="a">
+          <h1><img src={logo} className="App-logo" alt="logo"/></h1>
+        </div>
+          <div>
+            <ColorButton color='red' setColor={setColor}/>
+            <ColorButton color='orange' setColor={setColor}/>
+            <ColorButton color='yellow' setColor={setColor}/>
+          </div>
+
+          <div>
+            <ColorButton color='green' setColor={setColor}/>
+            <ColorButton color='blue' setColor={setColor}/>
+            <ColorButton color='indigo' setColor={setColor}/>
+          </div>
+          <ColorButton color='violet' setColor={setColor}/>
+          <ColorButton color='black' setColor={setColor}/>
+          <ColorButton color='white' setColor={setColor}/>
+        </div>
+      </div>
+  );
+}
+
+export default App;
